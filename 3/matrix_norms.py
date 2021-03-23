01e9fcefcf5d6bcad2c05b5b644ec346cbf25f0f
majchrow <dmajchrowski6@gmail.com>
2021-03-23 20:15:45 +0100

    Add inverse matrix calculation

--- 3/matrix_norms.py
+++ 3/matrix_norms.py
@@ -3,6 +3,34 @@ import numpy.linalg as la
 
 THRESHOLD = 10 ** (-5)
 
+# lab2
+def LU_factorization(A):
+    size = A.shape[0]
+    L = np.eye(size)
+    U = np.copy(A)
+    for i in np.arange(size - 1):
+        tmp = (U[i + 1:, i] / U[i, i]).copy()
+        L[i + 1:, i] = tmp
+        U[i + 1:, i:] -= np.tile(U[i, i:], (tmp.shape[0], 1)) * tmp.reshape([-1, 1])
+    return L, U
+
+def get_determinant(A):
+    _, U = LU_factorization(A)
+    return U.diagonal().prod()
+
+def get_coef(A):
+    coef = np.zeros(A.shape)
+    for x in range(A.shape[0]):
+        for y in range(A.shape[1]):
+            tmp_matrix = np.delete(A, x, axis=0)
+            tmp_matrix = np.delete(tmp_matrix, y, axis=1)
+            coef[x, y] = get_determinant(tmp_matrix)
+    return coef
+
+def get_inverse_matrix(A):
+    coef = get_coef(A)
+    det = get_determinant(A)
+    return coef.T / det
 
 def get_matrix(n):
     return np.random.rand(n, n)
@@ -50,13 +78,17 @@ def get_approximate_matrix_infinity_norm(A, n, samples=100000):
 
 if __name__ == "__main__":
     n = 3
-    p = 2
+    # p = 2
     # A = get_matrix(n)
     A = np.array(
        [[4, 9, 2],
         [3, 5, 7],
         [8, 1, 6]]
-    )
+    ).astype(float)
+
+    print('INVERSE      ', get_inverse_matrix(A))
+    print('INVERSE numpy', la.inv(A))
+
     print(f"A={A}")
     A_inv = la.inv(A)
     print(f"A^(-1)={A_inv}")
